85f9910e69835bcd4576122813f0a67a91439356
Дмитрий Ковалев <df.kovalev@gmail.com>
2016-11-17 05:46:27 +0200

    realeased 18 task

--- arrays_loops_tasks/18.php
+++ arrays_loops_tasks/18.php
@@ -10,17 +10,15 @@
 // выходные дни следует вывести жирным.
 
 $dayOfWeeks = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-$relaxDays = ['Суббота', 'Воскресенье'];
 
 foreach ($dayOfWeeks as $day){
-    $count++;
-    foreach ($relaxDays as $relaxDay){
-        if ($day == $relaxDay){
-            ($count == 7) ? print('<b>' . $day . '</b>') : print('<b>' . $day . '</b></br>');
-            $find = true;
-            break;
-        }
+    if ($day == 'Суббота') {
+        echo '<b>' . $day . '</b></br>';
+        continue;
     }
-    find ?
-    ($count == 11) ? print($month) : print($month . '</br>');
+    if ($day == 'Воскресенье') {
+        echo '<b>' . $day . '</b>';
+        break;
+    }
+    echo $day . '</br>';
 }
